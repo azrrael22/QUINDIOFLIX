d25f654d05211f1068b360f95b656c5dddbc6b23
Adriaann Mauricio Rodriguez Rubiano <149967949+azrrael22@users.noreply.github.com>
2026-05-06 10:18:28 -0500

    Agregar configuración inicial de Gradle y estructura del proyecto Spring Boot

---
+++ src/main/java/uniquindio/bases2/QUINDIOFLIX/QuindioflixApplication.java
@@ -0,0 +1,13 @@
+package uniquindio.bases2.QUINDIOFLIX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuindioflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuindioflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uniquindio/bases2/QUINDIOFLIX/QuindioflixApplicationTests.java
@@ -0,0 +1,13 @@
+package uniquindio.bases2.QUINDIOFLIX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuindioflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
